396ad81cc3ac15f54f58da8552d7ffd860284a14
Brannon Muncey <brmuncey@gmail.com>
2017-07-15 20:45:56 -0400

    Create Activities.

--- app/src/main/java/com/brmuncey/shortcutcalculator/View/MainActivity.java
+++ app/src/main/java/com/brmuncey/shortcutcalculator/View/MainActivity.java
@@ -1,13 +1,12 @@
 package com.brmuncey.shortcutcalculator.View;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.brmuncey.shortcutcalculator.R;
 
@@ -20,14 +19,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        addCalcButtons();
+    }
+
+    private void addCalcButtons() {
+        Button multiItemBtn = (Button) findViewById(R.id.multiItemBtn);
+        Button tipBtn = (Button) findViewById(R.id.tipBtn);
+        Button salesTaxBtn = (Button) findViewById(R.id.salesTaxBtn);
+
+        multiItemBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, MultiActivity.class));
             }
         });
+
+        tipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TipActivity.class));
+            }
+        });
+
+        salesTaxBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SalesTaxActivity.class));
+            }
+        });
+
     }
 
     @Override
@@ -35,20 +55,5 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
