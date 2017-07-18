b3094a7b01f98540f4f5fd30ccd611097c892b4d
Brannon Muncey <brmuncey@gmail.com>
2017-07-18 18:20:31 -0400

    Refactor Calculator

--- app/src/main/java/com/brmuncey/shortcutcalculator/Model/Calculator.java
+++ app/src/main/java/com/brmuncey/shortcutcalculator/Model/Calculator.java
@@ -4,17 +4,17 @@ public class Calculator {
 
     private double value;
 
-    public Calculator(){ value = 0; }
+    Calculator(){ value = 0; }
 
     public Calculator(double value) { this.value = value; }
 
-    public void add(double x) { value += x; }
+    public void add(double value) { this.value += value; }
 
-    public void subtract(double x) { value -= x; }
+    public void subtract(double value) { this.value -= value; }
 
-    public void divide(double x) { value = value / x;}
+    public void divide(double value) { this.value = this.value / value;}
 
-    public void multiply(double x) { value = value * x; }
+    public void multiply(double value) { this.value = this.value * value; }
 
     public double getValue() { return value; }
 }
